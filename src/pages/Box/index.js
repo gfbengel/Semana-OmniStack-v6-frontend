231be0260d05cb13a084d772a30345bc13ef30ab
Gabriel Engel <gfbengel@gmail.com>
2019-10-14 10:01:05 -0300

    frontend final

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import api from '../../services/api'
+import { formatDistance,parseISO } from 'date-fns'
+import pt from 'date-fns/locale/pt'
+import Dropzone from 'react-dropzone'
+import socket from 'socket.io-client'
 
-import {MDInsertDriveFile} from 'react-icons/md'
+import { MdInsertDriveFile } from 'react-icons/md'
 
 import logo from '../../assets/logo.svg'
 
@@ -8,29 +13,83 @@ import logo from '../../assets/logo.svg'
 import './styles.css';
 
 export default class Box extends Component {
+  state = {
+    box: {}
+  }
+  async componentDidMount(){
+    this.subscribeToNewFiles()
+
+    const box = this.props.match.params.id
+    const response = await api.get(`boxes/${box}`)
+
+    this.setState({box: response.data})
+  }
+
+  subscribeToNewFiles = () => {
+    const box = this.props.match.params.id
+    const io = socket('https://omni-v6-backend.herokuapp.com')
+
+    io.emit('connectRoom', box)
+
+
+    io.on('file', data => {
+      this.setState({
+        box: {
+          ...this.state.box, files: [data, ...this.state.box.files]
+        }
+      })
+    })
+  }
+
+  handleUpload = (files) => {
+    files.forEach(file => {
+
+      const data = new FormData()
+      const box = this.props.match.params.id
+
+      data.append('file', file)
+
+      api.post(`boxes/${box}/files`, data)
+
+    })
+  }
+
   render() {
     return (
       <div id="box-container">
         <header>
           <img src={logo} alt=""/>
-          <h1>gfbengel</h1>
+          <h1>{this.state.box.title}</h1>
         </header>
-
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({getRootProps, getInputProps}) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()}/>
+              <p>Arraste arquivos ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
         <ul>
-          <li>
-            <a href="">
-              <MDInsertDriveFile size={24} color="#a5cfff" />
-              <strong>nome.file</strong>
-            </a>
-            <span>a 3 minutos atras</span>
-          </li>
-          <li>
-            <a href="">
-              <MDInsertDriveFile size={24} color="#a5cfff" />
-              <strong>nome.file</strong>
-            </a>
-            <span>a 3 minutos atras</span>
-          </li>
+
+          { this.state.box.files && this.state.box.files.map(file => (
+            <li key={file._id}>
+              <a className="fileInfo" href={file.url} target="_blank" rel="noopener noreferrer">
+                <MdInsertDriveFile size={24} color="#a5cfff" />
+                <strong>{file.title}</strong>
+              </a>
+              <span>
+                há{" "}
+                {formatDistance(
+                  parseISO(file.createdAt), 
+                  new Date(), 
+                  {
+                    locale:pt
+                  }
+                )}
+              </span>
+            </li>
+          )) }
+
         </ul>
       </div>
     )
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL:'https://omni-v6-backend.herokuapp.com/',
+  baseURL:'https://omni-v6-backend.herokuapp.com',
 })
 
 export default api
